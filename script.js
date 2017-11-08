096f07b829f15fb9c69d012b3bcad332ff36e9f8
Zage <zagephillips@gmail.com>
2017-11-07 16:20:08 -0800

    change tabs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Post & Delete Buttons
 var button = document.getElementById("submit");
 var posted = 0;
 
@@ -15,6 +16,14 @@ button.addEventListener("click", function() {
 	var delete_button = document.createElement("button");
 	delete_button.innerHTML = "Delete";
 	delete_button.className = "delete";
+
+	delete_button.addEventListener("click", function() {
+		var retval = window.confirm("By pressing this button you are deleting this post permenantly.");
+		if (retval === true) {
+			posted--;
+			blog_post.remove();
+		} 
+	});
 
 	var bp1 = document.createElement("p");
 	var bp2 = document.createElement("p");
@@ -43,6 +52,7 @@ button.addEventListener("click", function() {
 	} else if (posted === 4) {
 		window.alert("You must delete one of your posts before posting again.");
 	} else {
+		// div.prepend(delete_button);
 		div.prepend(blog_post);
 		title.value = "";
 		text.value = "";
@@ -51,21 +61,34 @@ button.addEventListener("click", function() {
 	}	
 });
 
-// document.addEventListener("DOMContentLoaded", function() { 
-// var db = document.getElementsByClassName("delete");
-var db = document.getElementsByClassName("delete");
-if (db!== null) {
-db.addEventListener("click", function() {
-	// var retval = window.confirm("By pressing this button you are deleting this post permenantly.");
-	// if (retval === true) {
-	// 	posted--;
-	// 	// db.remove();
-	// } 
-	// else {
-		console.log("hey");
-	// }
-});
-};
-// };
+//Change Tabs
+function bindEventListeners() {
+	let menuItems = document.querySelectorAll("nav button");
+	for (let i = 0; i < menuItems.length; i++) {
+		menuItems[i].addEventListener("click", function() {
+			hideAll();
+			const className = menuItems[i].className;
+			const section = document.querySelector("section." + className);
+			unhide(section);
+			console.log(menuItems[i].className);
+		})
+	}
+}
+
+function hideAll() {
+	const sections = document.querySelectorAll("section.tabs");
+	for (let i = 0; i < sections.length; i++) {
+		if(!sections[i].className.match("hidden")) {
+			sections[i].className += " hidden"
+		}
+	}
+}
+
+function unhide(element) {
+	element.className = element.className.replace(" hidden", "");
+}
+
+//Driver Code
+bindEventListeners();
 
 
